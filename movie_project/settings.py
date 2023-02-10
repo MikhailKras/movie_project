777f796039531337594114fb966df37772edb62b
Mikhail Krasovitskiy <mikhailkrasovitskiy@gmail.com>
2023-02-10 09:21:13 +0300

    page with film list added

--- movie_project/settings.py
+++ movie_project/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'movie_app',
+    'django_extensions',
 ]
 
 MIDDLEWARE = [
